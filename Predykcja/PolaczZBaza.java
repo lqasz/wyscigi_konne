7726b1ff85575ef4f8971d06e534174ff19fa7a1
lqasz <lucas.osuch@wp.pl>
2017-01-28 19:27:47 +0100

    Zmiany v1.4

--- Predykcja/PolaczZBaza.java
+++ Predykcja/PolaczZBaza.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wyscigi_konne;
+package wyscigi_konne.Predykcja;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
